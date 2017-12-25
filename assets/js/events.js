bf62de1879298031d78866304da55fd46295120e
Kenzie Bottoms <kenziebottoms@gmail.com>
2017-12-24 19:23:04 -0600

    split input into search/edit modes, search mode works

--- assets/js/events.js
+++ assets/js/events.js
@@ -3,39 +3,75 @@
 const shopModule = require("./shops");
 const domController = require("./dom");
 
+// switch to search mode
 const activateSearch = () => {
-    $('#search').on("keyup", event => {
-         search(event.target.value);
-    });
-    $('#clear-search').on("click", event => {
-        $("#search").val("");
-        search("");
+    // TODO: deactivate edit
+    // activate search
+    $('#search').on("keyup", search);
+};
+
+// switch to edit mode
+const activateEdit = () => {
+    // deactivate search
+    $("#search").off("keyup", search);
+    // activate edit
+    $("#edit").on("click", event => {
+        // TODO: show edit form
     });
 };
 
-const search = term => {
+// listens for edit/search buttons
+const activateContentMode = () => {
+    activateSearch();
+    $('#content-mode label').on("click", event => {
+        let mode = $(event.target).children("input").attr("id");
+        if (mode == "edit") {
+            activateEdit();
+        } else {
+            activateSearch();
+        }
+    });
+};
+
+// callback for event listener created in activateSearch()
+const search = event => {
+    let term = event.target.value.trim().toLowerCase();
+    
+    // update title
     $("#search-div h5").text(`Results for "${term}"`);
+    // clear existing search results
     $("#search-results").html("");
+
+    // if the term is a word, show results; if not, hide them
     if (term != undefined && term != "") {
         $("#search-div").removeClass("hidden");
     } else {
         $("#search-div").addClass("hidden");
     }
+
+    // get and search shop list
     let shops = shopModule.getShops();
-    let results = shops.filter(shop => shop.name.toLowerCase().includes(term.toLowerCase()));
+    let results = shops.filter(shop => shop.name.toLowerCase().includes(term));
+    // if there are no results
     if (results.length == 0) {
-        results = shops.filter(shop => shop.desc.toLowerCase().includes(term.toLowerCase()));        
+        // try searching shop descriptions
+        results = shops.filter(shop => shop.desc.toLowerCase().includes(term));  
     }
+    // if there are still no results
     if (results.length == 0) {
-        results = shops.filter(shop => shop.region.toLowerCase().includes(term.toLowerCase()));        
+        // try searching region names
+        results = shops.filter(shop => shop.region.toLowerCase().includes(term));
     }
+
+    // add each shop to #search-results
     $.each(results, (index, result) => {
         domController.addSearchResult(result);
     });
 };
 
+// initializer called in main()
 const activateEventListeners = () => {
-    activateSearch();
+    activateContentMode();
 };
 
 module.exports = {activateEventListeners};
